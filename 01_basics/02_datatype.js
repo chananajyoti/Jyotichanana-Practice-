dcf79d0f8debb6c0f2443f5000781020f46c8fe9
Jyoti Chanana <154666452+chananajyoti@users.noreply.github.com>
2023-12-29 04:44:59 +0000

    Basic

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,8 @@
+let name = "Jyoti"
+let age = 20
+let loginid = true
+console.log(name)
+console.log(age)
+console.log(loginid)
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
